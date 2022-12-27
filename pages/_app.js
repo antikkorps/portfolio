169d0e6348dc2efe2347073825da1bff27c6ad82
antikkorps <franckvienot7@gmail.com>
2022-12-27 12:17:00 +0100

    start building my portefolio

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
