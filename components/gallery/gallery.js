753b5fed0e808ad86daad4bfbd96a2607ff75c27
antikkorps <franckvienot7@gmail.com>
2022-12-29 17:33:04 +0100

    added styles, changed the gallery item, added animation on hover images

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -1,284 +1,104 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Gallery() {
   return (
-    <div className='album py-5 bg-dark' id='gallery-portfolio'>
-      <div className='container mt-5 mb-5'>
-        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5'>
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=1'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
-
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
+    <>
+      <div className='gallery-container bg-dark'>
+        <div className='d-block text-center pt-5 pb-5'>
+          <h2 className='fw-light title-gallery'>
+            Bienvenue sur mon portefolio
+          </h2>
+          <p className='lead subtitle-gallery'>
+            Vous trouverez ici quelques exemples de réalisations:
+          </p>
+        </div>
+        <div className='section-fluid-main bg-dark'>
+          <div className='section-row'>
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
                 </div>
               </div>
             </div>
-          </div>
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=2'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
 
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
                 </div>
               </div>
             </div>
-          </div>
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=3'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
 
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className='section-col'>
+              <div className='section'>
+                <div className='section-in'>
+                  <Image
+                    src='/images/contact.jpg'
+                    alt=''
+                    width={400}
+                    height={200}
+                  />
                 </div>
               </div>
             </div>
           </div>
 
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=4'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
-
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=5'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
-
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='col'>
-            <div className='card shadow-sm'>
-              <svg
-                className='bd-placeholder-img card-img-top'
-                width='100%'
-                height='225'
-                xmlns='https://picsum.photos/200/300?random=6'
-                role='img'
-                aria-label='Placeholder: Thumbnail'
-                preserveAspectRatio='xMidYMid slice'
-                focusable='false'
-              >
-                <title>Placeholder</title>
-                <rect width='100%' height='100%' fill='#55595c'></rect>
-                <text x='50%' y='50%' fill='#eceeef' dy='.3em'>
-                  Thumbnail
-                </text>
-              </svg>
-
-              <div className='card-body'>
-                <p className='card-text'>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div className='d-flex justify-content-between align-items-center'>
-                  <div className='btn-group'>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      View
-                    </button>
-                    <button
-                      type='button'
-                      className='btn btn-sm btn-outline-secondary'
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small className='text-muted'>9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
+          <h2 className='d-flex justify-content-center subtitle-gallery'>
+            Et tellement d&#39;autres...
+          </h2>
         </div>
       </div>
-      <h2 className='d-flex justify-content-center subtitle-gallery'>
-        Et tellement d&#39;autres...
-      </h2>
-    </div>
+    </>
   );
 }
--- components/maintop/maintop.js
+++ components/maintop/maintop.js
@@ -2,26 +2,30 @@ import Link from 'next/link';
 
 export default function Maintop() {
   return (
-    <section className='py-5 text-center container maintop'>
-      <div className='row py-lg-5'>
-        <div className='col-lg-6 col-md-8 mx-auto'>
-          <h1 className='fw-light'>Bienvenue sur mon portefolio</h1>
-          <p className='lead text-muted'>
-            Vous trouverez ici quelques exemples de réalisations.
-          </p>
-          <p>
-            <Link
-              href='#gallery-portfolio'
-              className='btn btn-secondary my-2 mx-2'
-            >
-              Voir les réalisations
-            </Link>
-            <Link href='/contact/form' className='btn btn-primary my-2 mx-2'>
-              Contactez-moi!
-            </Link>
-          </p>
-        </div>
+    <>
+      <div className='container' id='maintop'>
+        <section className='py-5 text-center container maintop'>
+          <div className='row py-lg-5' id='mainHeadline'>
+            <div className='col-lg-6 col-md-8 mx-auto'>
+              <h1 className='fw-light'>Bienvenue sur mon portefolio</h1>
+              <p className='lead text-muted'>
+                Vous trouverez ici quelques exemples de réalisations.
+              </p>
+              <p>
+                <Link href='#gallery-portfolio' className='btn btn-secondary'>
+                  Voir les réalisations
+                </Link>
+                <Link
+                  href='/contact/form'
+                  className='btn btn-primary my-2 mx-2'
+                >
+                  Contactez-moi!
+                </Link>
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
+    </>
   );
 }
